f42d37de5ba20092313436ba48847387c51eb2ae
MikeFox303 <167519861+MikeFox303@users.noreply.github.com>
2026-08-20 23:08:26 +0300

    test: enforce modern legacy separation and slot fallback

--- tests/test_consumption_protocol.py
+++ tests/test_consumption_protocol.py
@@ -58,3 +58,33 @@ async def _resolved(value, _expected):
 
 async def _record(calls, spool, grams, kw):
     calls.append((spool, grams, kw))
+
+
+@pytest.mark.asyncio
+async def test_modern_print_complete_does_not_consume():
+    driver = object.__new__(Driver)
+    driver._spoolman_enabled = False
+    driver._slot_to_filaman_spool = {"0-0": 42}
+    driver._report_consumption = lambda *args, **kwargs: _record([], *args, **kwargs)
+
+    await driver._handle_print_complete({"weight_used": 12.4}, event_id="run-1")
+
+
+@pytest.mark.asyncio
+async def test_modern_usage_without_bambuddy_spool_id_uses_slot_fallback():
+    driver = object.__new__(Driver)
+    driver._spoolman_enabled = False
+    driver._modern_usage_event_ids = set()
+    driver._legacy_consumption_tasks = {}
+    driver._slot_to_filaman_spool = {"255-1": 99}
+    driver._resolve_bambuddy_spool_id = lambda _: _resolved(None, None)
+    calls = []
+    driver._report_consumption = lambda spool, grams, **kw: _record(calls, spool, grams, kw)
+
+    await driver._handle_spool_usage_logged({
+        "printer_id": 3,
+        "event_id": "run-2",
+        "usage": [{"weight_used": 3.2, "ams_id": 255, "tray_id": 1}],
+    })
+
+    assert calls == [(99, 3.2, {"source_event_key": "run-2:None:255:1"})]
